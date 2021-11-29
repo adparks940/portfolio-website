4d7a617ff3ad5b0f921054725d7308bd8f487cf9
adparks940 <87881682+adparks940@users.noreply.github.com>
2021-11-28 22:50:24 -0600

    Styled header
    Added jQuery capabilities and a hamburger menu that toggles on and off

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
 console.log("Welcome to my website")
+
+function menuToggle() {}
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+}
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  x.className += ' responsive';
+}
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
